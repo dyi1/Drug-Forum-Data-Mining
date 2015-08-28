efa6a8b0ce1eb24aba297237b2c47f34c98bf404
DavidYi1 <yid@bxscience.edu>
2015-08-28 00:34:17 -0400

    Attempted to Scrape all experience
    Some eluded because not in ASCII range

--- Erowid/Erowid/pipelines.py
+++ Erowid/Erowid/pipelines.py
@@ -4,8 +4,21 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import os
 
 class ErowidPipeline(object):
     def process_item(self, item, spider):
-        return item
+    	cargo =  {'title': item['Title'](),
+    			'author': item['Author'](),
+    			 'text': item['Text'](),
+    			 'substance': item['Substance']()}
+
+    	directory = os.path.join('Erowid/archive/',cargo['substance'][0].strip().lower())
+    	filename = os.path.join(directory,cargo['title'][0]+'.txt')
+
+    	if not os.path.exists(directory):
+    		os.makedirs(directory)
+
+    	with open(filename,'wb') as fid:
+    		for symbols in cargo['text']:
+    			print>>fid,symbols 
--- Erowid/Erowid/spiders/experiences.py
+++ Erowid/Erowid/spiders/experiences.py
@@ -1,76 +1,29 @@
+
+
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.selector import HtmlXPathSelector
-from scrapy.http import Request
+
 from Erowid.items import ErowidItem
-import os
+
 
 class ExperiencesSpider(CrawlSpider):
     name = "experiences"
     allowed_domains = ["www.erowid.org"]
     start_urls = ['https://www.erowid.org/experiences/exp_list.shtml']
-    
-    rules = [Rule(LinkExtractor(allow =('subs/exp_[a-zA-Z]+.shtml')),callback='parse_item', follow = True)
-
-#        Rule(LinkExtractor(allow =('subs/exp_[a-zA-Z]+.shtml')), follow = True)    
-
-   ]
+    rules = [ 
+        Rule(LinkExtractor(allow =('subs/exp_[a-zA-Z]+.shtml')), follow = True), 
+        Rule(LinkExtractor(allow=r'/experiences/exp\.php\?ID=\d+$'),callback='parse_item', follow = True)
+    ]
     def parse_item(self, response):
-        filename = str(response.url)[44:-6]
-        selectors = response.css('table')
-        if not os.path.exists('drugs-%s' % (filename)): ##Make the file
-            os.makedirs('drugs-%s' % (filename))
-        list_of_experience = selectors.xpath('//table[@class="exp-cat-table"]/tr/td/a/@href').extract()
-        
-        for item in list_of_experience:
-            request_url = str(item)
-            
-            Request(url="http://www.erowid.org" + request_url, callback = 'request_experience')
-            
-            def request_experience(self, response):
-                selectors = response.css('div')
-                
-                for selector in selectors:
-                    experience = ErowidItem()
-                    experience['Author'] = selector.xpath('//div[@class="author"]/a/text()').extract()
-                    experience['Title'] = selector.xpath('//div[@class="title"]/text()').extract()
-                    experience['Substance'] = selector.xpath('//div[@class="substance"]/text()').extract()
-                    experience['Text'] = selector.xpath("//div[@class = 'report-text-surround']/text()").extract()
-                    ##use strip to remove extra spaces
-                    ##[3:-2] Removes the extra parts in the beginning
-                    experience['Author'] = str(experience['Author'])[3:-2].strip()
-                    experience['Substance'] = str(experience['Substance'])[3:-2].strip()
-                    experience['Title'] = str(experience['Title'])[3:-2].strip()
-                    ##Removed non-text and formatting 
-                    experience['Text'] = str(experience['Text'])[3:-2].replace("\\n\'", "")
-                    experience['Text'] = str(experience['Text']).replace('"', "")
-                    experience['Text'] = str(experience['Text']).replace(", u'", "")
-                    experience['Text'] = str(experience['Text']).replace(", u\\n'", "")
-                    experience['Text'] = str(experience['Text']).replace(", u\\n", "")
-                    experience['Text'] = str(experience['Text']).replace("'\\n", "")
-                    experience['Text'] = str(experience['Text']).replace("\\n", "")
-                    experience['Text'] = str(experience['Text']).replace("\\r'", "")
-                    experience['Text'] = str(experience['Text']).replace("\\r", "")
-                    experience['Text'] = str(experience['Text']).replace("\\t'", "")
-                    experience['Text'] = str(experience['Text']).replace("\\t", "")
-                    experience['Text'] = str(experience['Text']).replace(", u", "")
-            
-            
-                    title = str(experience['Substance']) + " "+ str(experience['Title'])
-                    print experience
-                    with open(os.path.join('drugs-%s' % (filename), title),"w") as fid:
-                        fid.write(str(experience) + "\n")            
-                
-                
-        
-        #sos.chdir(os.pardir)##Back to previous file
-        '''
-        
-        for item in list_of_experience:
-            with open(os.path.join('drugs-%s' % (filename), filename),"a") as fid:
-                fid.write(item + "\n")
-            
+        selectors = response.css('div')
+        for selector in selectors:
+            experience = ErowidItem()
+            experience['Author'] = selector.xpath('//div[@class="author"]/a/text()').extract
+            experience['Title'] = selector.xpath('//div[@class="title"]/text()').extract
+            experience['Substance'] = selector.xpath('//div[@class="substance"]/text()').extract
+            experience['Text'] = selector.xpath("//div[@class = 'report-text-surround']/text()").extract
 
+        yield experience
        ##Will move the code over from test.py after
-        '''
