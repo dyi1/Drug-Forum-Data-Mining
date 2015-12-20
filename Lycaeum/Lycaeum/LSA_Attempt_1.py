44b018a0adb01fea48b1427c6e176eab31381646
DavidYi1 <yid@bxscience.edu>
2015-12-20 14:32:50 -0500

    Make Tf-Idf and LSA model

--- Lycaeum/Lycaeum/LSA_Attempt_1.py
+++ Lycaeum/Lycaeum/LSA_Attempt_1.py
@@ -1,27 +1,37 @@
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-from gensim import corpora, models, 
-from nltk.corpus import stopwords
+from gensim import corpora, models
 import codecs
 
-documents = []
-with codecs.open("Master_File_Forums.txt", encoding = 'utf-8', mode= "r") as fid:
-   for line in fid:
-       documents.append(line)
-stoplist = []
-x = stopwords.words('english')
-for word in x:
-    stoplist.append(word)
+dictionary = corpora.Dictionary(line.lower().split() for line in codecs.open('deduplicated-curated-drug-names', encoding = 'utf-8', mode= "r" ))
 
-##Removes Stopwords
-texts = [[word for word in document.lower().split() if word not in stoplist] for document in documents]
-
-dictionary = corpora.Dictionary(texts)
-corpus = [dictionary.doc2bow(text) for text in texts]
 print dictionary
-print corpus
-
+    
+class MyCorpus(object):   
+    def __iter__(self):
+        for line in codecs.open('Master_File_Forums.txt', encoding = 'utf-8', mode= "r"):
+            drug_phrases = []
+            for word in line:
+                if word in dictionary:
+                    drug_phrases.append(word)
+            yield dictionary.doc2bow(drug_phrases)
+            
+corpus = MyCorpus()
+print corpus 
+'''
+for vector in corpus: # load one vector into memory at a time
+     print(vector)
+'''
 tfidf = models.TfidfModel(corpus)
+#tfidf.save('/tmp/model.tfidf')
+corpus_tfidf = tfidf[corpus]
+
+for doc in corpus_tfidf:
+    print(doc)
     
-    
-    
+lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=2)
+#lsi.save('/tmp/model.lsi')
+lsi_corpus = lsi[corpus_tfidf]
+
+for doc in lsi_corpus:
+    print doc
